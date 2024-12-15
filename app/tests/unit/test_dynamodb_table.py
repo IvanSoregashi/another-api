8b838cdfba04bb224ca3b6d7946589140a7b348f
Ivan Soregashi <IvanShiriaev2222@gmail.com>
2024-12-15 18:48:19 +0500

    starting on db tests

--- app/tests/unit/test_dynamodb_table.py
+++ app/tests/unit/test_dynamodb_table.py
@@ -1,9 +1,29 @@
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import patch, Mock, MagicMock
 
-from app.db.DynamoDB import DynamoDB
+from app.db.DynamoDB import DynamoDBTable
 
 
-class TestDynamoDB(TestCase):
-    def test_put_empty_transaction(self):
-        pass
+class TestDynamoDBTable(TestCase):
+
+    def test_table_initialization_with_db_arg(self):
+        dynamodb = Mock()
+        dynamodb.Table.return_value = "test_table"
+
+        table = DynamoDBTable("Test", dynamodb)
+
+        dynamodb.Table.assert_called_with("Test")
+        dynamodb.Table.assert_called_once()
+        self.assertEqual(table.table, "test_table")
+
+    @patch("app.db.DynamoDB.get_dynamodb_resource")
+    def test_table_initialization_without_db_arg(self, mock_get_resource):
+        dynamodb = Mock()
+        dynamodb.Table.return_value = "test_table"
+        mock_get_resource.return_value = dynamodb
+
+        table = DynamoDBTable("Test")
+
+        dynamodb.Table.assert_called_with("Test")
+        dynamodb.Table.assert_called_once()
+        self.assertEqual(table.table, "test_table")
