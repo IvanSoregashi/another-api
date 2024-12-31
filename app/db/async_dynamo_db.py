faba868e1a9a79941daec91307f93103a731788d
Ivan Soregashi <IvanShiriaev2222@gmail.com>
2024-12-31 17:55:19 +0500

    A little bit of refactor to conform to repository pattern

--- app/db/async_dynamo_db.py
+++ app/db/async_dynamo_db.py
@@ -1,13 +1,12 @@
 import os
 import contextlib
-import asyncio
-from functools import reduce
+# from functools import reduce
 from typing import Self
 
 
 import aioboto3
-from botocore.exceptions import ClientError
-from boto3.dynamodb.conditions import Key, And
+# from botocore.exceptions import ClientError
+# from boto3.dynamodb.conditions import Key, And
 
 
 class DBError(Exception):
@@ -52,97 +51,12 @@ class AWS:
         self.resources[resource_name] = resource
         return resource
 
+    async def get_dynamo_db_table(self, table_name: str) -> Self:
+        dynamodb = await self.get_async_resource("dynamodb")
+        table = await dynamodb.Table(table_name)
+        return table
+
     async def cleanup(self):
         self.resources = dict()
         await self.stack.aclose()
 
-
-class DynamoDBTable:
-    def __init__(self, table):
-        """
-        Initialize a DynamoDB table interface.
-        """
-        self.table = table
-
-    @classmethod
-    async def named(cls, aws_object: AWS, table_name: str) -> Self:
-        dynamodb = await aws_object.get_async_resource("dynamodb")
-        table = await dynamodb.Table(table_name)
-        return cls(table)
-
-    async def pull_item(self, key: dict) -> dict:
-        """
-        Retrieve an item by its primary key.
-        """
-        try:
-            response = await self.table.get_item(Key=key)
-            return response.get("Item", {})
-        except ClientError as e:
-            error_message = e.response["Error"]["Message"]
-            raise DBError(f"Error putting item: {error_message}")
-
-    async def query_items(self, key: str, value: str) -> dict:
-        """
-        Query items by primary key.
-        """
-        try:
-            response = await self.table.query(KeyConditionExpression=Key(key).eq(value))
-            return response.get("Items", [])
-        except ClientError as e:
-            error_message = e.response["Error"]["Message"]
-            raise DBError(f"Error putting item: {error_message}")
-
-    async def put_item(self, item: dict) -> dict:
-        """
-        Insert or overwrite an item.
-        """
-        try:
-            await self.table.put_item(Item=item)
-            return item
-        except ClientError as e:
-            error_message = e.response["Error"]["Message"]
-            raise DBError(f"Error putting item: {error_message}")
-
-    async def delete_item(self, key: dict) -> None:
-        """
-        Delete an item by its primary key.
-        """
-        try:
-            await self.table.delete_item(Key=key)
-        except ClientError as e:
-            error_message = e.response["Error"]["Message"]
-            raise DBError(f"Error putting item: {error_message}")
-
-    async def scan_table(self, filters: dict) -> list:
-        """
-        Scan the table and return all items.
-        """
-        # Should I add Limit???
-        try:
-            filters = {"FilterExpression": reduce(And, ([Key(k).eq(v) for k, v in filters.items()]))} if filters else {}
-            response = await self.table.scan(**filters)
-            return response.get("Items", [])
-        except ClientError as e:
-            error_message = e.response["Error"]["Message"]
-            raise DBError(f"Error putting item: {error_message}")
-
-
-async def main():
-    stack = contextlib.AsyncExitStack()
-    session = aioboto3.Session(**AWSConfig().to_dict())
-
-    sqs = await stack.enter_async_context(session.resource(service_name="dynamodb"))
-    #... pass sqs object around, into other async functions etc..
-
-    # before you exit, clean up
-    await stack.aclose()
-
-
-if __name__ == '__main__':
-    aws1 = AWS(aws_access_key_id="test_id", aws_secret_access_key="test_key", region_name="test_region")
-    print(f"AWS 1 ID: {id(aws1)}")
-
-    aws2 = AWS()  # Notice we don't need to provide credentials again
-    print(f"AWS 2 ID: {id(aws2)}")
-
-    print(aws1 is aws2)  # Output: True
---
+++ app/repository/dynamo_db.py
@@ -0,0 +1,79 @@
+from functools import reduce
+
+from boto3.dynamodb.conditions import Key, And
+from botocore.exceptions import ClientError
+
+from app.db.async_dynamo_db import DBError, AWS
+from app.repository.repository import AbstractRepository
+
+
+class DynamoDBRepository(AbstractRepository):
+    def __init__(self, table):
+        """
+        Initialize a DynamoDB table interface.
+        """
+        self.table = table
+
+    @classmethod
+    async def initialize(cls, aws_object: AWS, table_name):
+        """
+        Async Initialization for the table.
+        """
+        table = await aws_object.get_dynamo_db_table(table_name)
+        return cls(table)
+
+    async def scan_table(self, filters: dict) -> list:
+        """
+        Scan the table and return all items.
+        """
+        # TODO add limit
+        try:
+            filters = {"FilterExpression": reduce(And, ([Key(k).eq(v) for k, v in filters.items()]))} if filters else {}
+            response = await self.table.scan(**filters)
+            return response.get("Items", [])
+        except ClientError as e:
+            error_message = e.response["Error"]["Message"]
+            raise DBError(f"Error putting item: {error_message}")
+
+    async def query_items(self, key, value) -> list:
+        """
+        Query items by primary key.
+        """
+        try:
+            response = await self.table.query(KeyConditionExpression=Key(key).eq(value))
+            return response.get("Items", [])
+        except ClientError as e:
+            error_message = e.response["Error"]["Message"]
+            raise DBError(f"Error putting item: {error_message}")
+
+    async def put_item(self, item: dict) -> dict:
+        """
+        Insert or overwrite an item.
+        """
+        try:
+            await self.table.put_item(Item=item)
+            return item
+        except ClientError as e:
+            error_message = e.response["Error"]["Message"]
+            raise DBError(f"Error putting item: {error_message}")
+
+    async def get_item(self, keys: dict) -> dict:
+        """
+        Retrieve an item by its primary key.
+        """
+        try:
+            response = await self.table.get_item(Key=keys)
+            return response.get("Item", {})
+        except ClientError as e:
+            error_message = e.response["Error"]["Message"]
+            raise DBError(f"Error putting item: {error_message}")
+
+    async def delete_item(self, keys: dict) -> None:
+        """
+        Delete an item by its primary key.
+        """
+        try:
+            await self.table.delete_item(Key=keys)
+        except ClientError as e:
+            error_message = e.response["Error"]["Message"]
+            raise DBError(f"Error putting item: {error_message}")
