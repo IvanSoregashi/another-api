30bbf6fbbd6f6ac78bf6708cf6e96fcc87bed213
Ivan Soregashi <IvanShiriaev2222@gmail.com>
2025-01-02 23:34:52 +0500

    Fixed the integration tests, on second thought, these might just be unit tests for endpoints

--- tests/integration/test_transactions_api_use_case.py
+++ tests/integration/test_transactions_api_use_case.py
@@ -3,20 +3,21 @@ from unittest.mock import AsyncMock
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
-from app.routers.transactions import get_repo
+from app.dependencies import get_transaction_service
+from app.models.transactions import TransactionQuery, Transaction
 
 client = TestClient(app)
 
 
 @pytest.fixture
-def test_repo():
-    test_repo = AsyncMock()
-    app.dependency_overrides[get_repo] = lambda: test_repo
-    return test_repo
+def test_service():
+    test_service = AsyncMock()
+    app.dependency_overrides[get_transaction_service] = lambda: test_service
+    return test_service
 
 
-def test_scan_all_transactions(test_repo):
-    test_repo.scan_table.return_value = [{
+def test_scan_all_transactions(test_service):
+    test_service.scan.return_value = [{
         "month": "2024-12",
         "transaction_id": "f1bf8fed-8cb5-4403-a3a1-7b2df262182b",
         "datetime": "2024-12-25T05:06:04.288542+00:00",
@@ -32,7 +33,9 @@ def test_scan_all_transactions(test_repo):
 
     response = client.get("/transactions", params={})
 
-    test_repo.scan_table.assert_awaited_once_with({})
+    model = TransactionQuery.model_validate({})
+
+    test_service.scan.assert_awaited_once_with(model)
     assert response.status_code == 200
     assert response.json() == [{
         "month": "2024-12",
@@ -49,48 +52,54 @@ def test_scan_all_transactions(test_repo):
     }]
 
 
-def test_scan_all_transactions_empty_response(test_repo):
-    test_repo.scan_table.return_value = []
+def test_scan_all_transactions_empty_response(test_service):
+    test_service.scan.return_value = []
 
     response = client.get("/transactions", params={})
 
-    test_repo.scan_table.assert_awaited_once_with({})
+    model = TransactionQuery.model_validate({})
+
+    test_service.scan.assert_awaited_once_with(model)
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_scan_all_transactions_with_type_filter(test_repo):
-    test_repo.scan_table.return_value = []
+def test_scan_all_transactions_with_type_filter(test_service):
+    test_service.scan.return_value = []
 
     response = client.get("/transactions", params={"type": "Transfer"})
 
-    test_repo.scan_table.assert_awaited_once_with({"type": "Transfer"})
+    model = TransactionQuery.model_validate({"type": "Transfer"})
+
+    test_service.scan.assert_awaited_once_with(model)
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_scan_all_transactions_with_wrong_filter(test_repo):
-    test_repo.scan_table.return_value = []
+def test_scan_all_transactions_with_wrong_filter(test_service):
+    test_service.scan.return_value = []
 
     response = client.get("/transactions", params={"sdf": "Transfer"})
 
-    test_repo.scan_table.assert_awaited_once_with({})
+    model = TransactionQuery.model_validate({})
+
+    test_service.scan.assert_awaited_once_with(model)
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_query_monthly_transactions_empty_response(test_repo):
-    test_repo.query_items.return_value = []
+def test_query_monthly_transactions_empty_response(test_service):
+    test_service.query.return_value = []
 
     response = client.get("/transactions/1900-01")
 
-    test_repo.query_items.assert_awaited_once_with("month", "1900-01")
+    test_service.query.assert_awaited_once_with("1900-01")
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_query_monthly_transactions_double(test_repo):
-    test_repo.query_items.return_value = [
+def test_query_monthly_transactions_double(test_service):
+    test_service.query.return_value = [
         {
             "month": "2024-12",
             "transaction_id": "f1bf8fed-8cb5-4403-a3a1-7b2df262182b",
@@ -120,7 +129,7 @@ def test_query_monthly_transactions_double(test_repo):
 
     response = client.get("/transactions/1900-01")
 
-    test_repo.query_items.assert_awaited_once_with("month", "1900-01")
+    test_service.query.assert_awaited_once_with("1900-01")
     assert response.status_code == 200
     assert response.json() == [
         {
@@ -154,8 +163,8 @@ def test_query_monthly_transactions_double(test_repo):
 # TODO Query Filter Query Support
 
 
-def test_get_transaction_found(test_repo):
-    test_repo.pull_item.return_value = {
+def test_get_transaction_found(test_service):
+    test_service.get.return_value = {
             "month": "2024-12",
             "transaction_id": "f1bf8fed-8cb5-4403-a3a1-7b2df262182b",
             "datetime": "2024-12-25T05:06:04.288542+00:00",
@@ -171,10 +180,7 @@ def test_get_transaction_found(test_repo):
 
     response = client.get("/transactions/1900-01/f1bf8fed-8cb5-4403-a3a1-7b2df262182b")
 
-    test_repo.pull_item.assert_awaited_once_with(
-        {"month": "1900-01",
-         "transaction_id": "f1bf8fed-8cb5-4403-a3a1-7b2df262182b"}
-    )
+    test_service.get.assert_awaited_once_with("1900-01", "f1bf8fed-8cb5-4403-a3a1-7b2df262182b")
 
     assert response.status_code == 200
     assert response.json() == {
@@ -192,21 +198,18 @@ def test_get_transaction_found(test_repo):
         }
 
 
-def test_get_transaction_not_found(test_repo):
-    test_repo.pull_item.return_value = {}
+def test_get_transaction_not_found(test_service):
+    test_service.get.return_value = {}
 
     response = client.get("/transactions/1900-01/f1bf8fed-8cb5-4403-a3a1-7b2df262182b")
 
-    test_repo.pull_item.assert_awaited_once_with(
-        {"month": "1900-01",
-         "transaction_id": "f1bf8fed-8cb5-4403-a3a1-7b2df262182b"}
-    )
+    test_service.get.assert_awaited_once_with("1900-01", "f1bf8fed-8cb5-4403-a3a1-7b2df262182b")
 
     assert response.status_code == 404
     assert response.json() == {"detail": "Item not found"}
 
 
-def test_create_correct_transaction(test_repo):
+def test_create_correct_transaction(test_service):
     data = {
         "month": "2024-12",
         "transaction_id": "f1bf8fed-8cb5-4403-a3a1-7b2df262182b",
@@ -221,11 +224,13 @@ def test_create_correct_transaction(test_repo):
         "comment": None
     }
 
-    test_repo.put_item.return_value = data
+    test_service.put.return_value = data
 
     response = client.post("/transactions", json=data)
 
-    test_repo.put_item.assert_awaited_once_with(data)
+    model = Transaction.model_validate(data)
+
+    test_service.put.assert_awaited_once_with(model)
 
     assert response.status_code == 201
     assert response.json() == {
@@ -243,7 +248,7 @@ def test_create_correct_transaction(test_repo):
     }
 
 
-def test_create_incorrect_transaction(test_repo):
+def test_create_incorrect_transaction(test_service):
     data = {
         "month": "2024-12",
         "transactddionid": "f1bf8fed-8cb5-4403-a3a1-7b2df262182b",
@@ -258,25 +263,22 @@ def test_create_incorrect_transaction(test_repo):
         "comment": None
     }
 
-    test_repo.put_item.return_value = data
+    test_service.put.return_value = data
 
     response = client.post("/transactions", json=data)
 
-    test_repo.put_item.assert_not_awaited()
+    test_service.put.assert_not_awaited()
 
     assert response.status_code == 422
 #    assert response.json() == {}
 
 
-def test_delete_transaction(test_repo):
-    test_repo.delete_item.return_value = None
+def test_delete_transaction(test_service):
+    test_service.delete.return_value = None
 
     response = client.delete("/transactions/1900-01/f1bf8fed-8cb5-4403-a3a1-7b2df262182b")
 
-    test_repo.delete_item.assert_awaited_once_with(
-        {"month": "1900-01",
-         "transaction_id": "f1bf8fed-8cb5-4403-a3a1-7b2df262182b"}
-    )
+    test_service.delete.assert_awaited_once_with("1900-01", "f1bf8fed-8cb5-4403-a3a1-7b2df262182b")
 
     assert response.status_code == 204
     assert response.content == b''
