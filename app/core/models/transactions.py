3ae6712e11741359c7e99867a9ca3ba5fa8d52d1
Ivan Soregashi <IvanShiriaev2222@gmail.com>
2025-01-16 21:21:56 +0500

    added prototypr TransferTransaction

--- app/core/models/transactions.py
+++ app/core/models/transactions.py
@@ -40,6 +40,51 @@ class Transaction(BaseModel):
             self.category = point_to_cat.get(self.point)
 
 
+class TranferTransaction(BaseModel):
+    transaction_id: str | None = Field(default_factory=lambda: str(uuid4()),
+                                       description="Unique transaction identifier")
+    month: str | None = None  # Field(default_factory=current_year_month, description="Date in YYYY-MM format")
+    datetime: str | None = Field(default_factory=timestamp, description="Timestamp in ISO format, UTC TimeZone")
+    type: str | None = "Transfer"
+    from_account: str
+    to_account: str
+    from_currency: str | None = None
+    to_currency: str | None = None
+    from_amount: Decimal
+    to_amount: Decimal
+    category: str | None = None
+    point: str
+    item: str | None = None
+    comment: str | None = None
+
+    def divide(self):
+        _from = Transaction(
+            month=self.month,
+            datetime=self.datetime,
+            type=self.type,
+            account=self.from_account,
+            currency=self.from_currency,
+            amount=self.from_amount,
+            category=self.category,
+            point=self.point,
+            item=self.item,
+            comment=self.comment,
+        )
+        _to = Transaction(
+            month=self.month,
+            datetime=self.datetime,
+            type=self.type,
+            account=self.to_account,
+            currency=self.to_currency,
+            amount=self.to_amount,
+            category=self.category,
+            point=self.point,
+            item=self.item,
+            comment=self.comment,
+        )
+        return _from, _to
+
+
 class TransactionQuery(BaseModel):  # Transaction):
     # TODO Investigate how to make all the fields optional properly
     type: Literal["Expense", "Income", "Transfer"] | None = None
